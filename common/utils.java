cda0d87c3e38a3c55c5e24f3c807c6edc2514db8
LitoleNINJA <74725722+LitoleNINJA@users.noreply.github.com>
2025-12-04 07:29:04 +0000

    day4 part1 :(

--- common/utils.java
+++ common/utils.java
@@ -7,10 +7,18 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.Instant;
+import java.util.ArrayList;
 
 public class utils {
     public static final String INPUT_FILENAME = "input.txt";
     public static final String OUTPUT_FILENAME = "output.txt";
+
+    // 8 directions: N, NE, E, SE, S, SW, W, NW
+    public static final int[][] DIRECTIONS_8 = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},           {0, 1},
+        {1, -1},  {1, 0},  {1, 1}
+    };
 
     public static PrintStream initializeOutputStream() {
         try {
@@ -26,6 +34,31 @@ public class utils {
     }
 
     public static long getCurrentTime() {
-        return Instant.now().getNano() / 1000;
+        return System.currentTimeMillis();
+    }
+    public static void printTimeStats(PrintStream out, long processStartTime, long startTime, long endTime) {
+        out.println("\n---------------------------------");
+        out.printf("%-20s : %6d ms%n", "Input read time", processStartTime - startTime);
+        out.printf("%-20s : %6d ms%n", "Process time", endTime - processStartTime);
+        out.printf("%-20s : %6d ms%n", "Total time", endTime - startTime);
+        out.println("---------------------------------");
+    }
+
+    public static ArrayList<String> getInputLines() throws IOException {
+        BufferedReader reader = initilaizeReader();
+        ArrayList<String> lines = new ArrayList<>();
+
+        String line = reader.readLine();
+        while (line != null) {
+            lines.add(line);
+            line = reader.readLine();
+        }
+        reader.close();
+
+        return lines;
+    }
+
+    public static boolean isInGrid(int i, int j, int rows, int cols) {
+        return i >= 0 && i < rows && j >= 0 && j < cols;
     }
 }
--- day4/PrintingDepartment.java
+++ day4/PrintingDepartment.java
@@ -1,5 +1,55 @@
 package day4;
 
+import common.utils;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+
 public class PrintingDepartment {
+    public static PrintStream out = utils.initializeOutputStream();
+    public static void main(String[] args) {
+        try {
+            long startTime = utils.getCurrentTime();
+            
+            ArrayList<String> lines = utils.getInputLines();
+            int n = lines.size(), m = lines.getFirst().length();
+            
+            long processStartTime = utils.getCurrentTime();
+            int ans = 0;
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<m; j++) {
+                    if(lines.get(i).charAt(j) == '@') {
+                        int count = countAdjacentRolls(lines, i, j, n, m); 
+                        if(count < 4) {
+                            // out.printf("Roll found at i=%d, j=%d\n", i, j);
+                            ans++;
+                        }
+                    }
+                }
+            }
+            long endTime = utils.getCurrentTime();
+            
+            utils.printTimeStats(out, processStartTime, startTime, endTime);
+            
+            System.out.println("Answer : " + ans);
+            out.printf("Answer : %d", ans);
+        } catch (Exception e) {
+            System.out.println("Exception : " + e);
+        }
+    }
+
+    private static int countAdjacentRolls(ArrayList<String> lines, int i, int j, int n, int m) {
+        int count = 0;
         
+        for(int[] dir : utils.DIRECTIONS_8) {
+            int new_i = i + dir[0], new_j = j + dir[1];
+            if (utils.isInGrid(new_i, new_j, n, m)) {
+                if (lines.get(new_i).charAt(new_j) == '@') {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
