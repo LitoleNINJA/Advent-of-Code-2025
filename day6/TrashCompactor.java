f3615dc5a9a2bab4ec9e9f6742f3066217f786e6
LitoleNINJA <74725722+LitoleNINJA@users.noreply.github.com>
2025-12-06 15:55:15 +0000

    day6 part1: just implement

---
+++ day6/TrashCompactor.java
@@ -0,0 +1,55 @@
+package day6;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import common.utils;
+
+public class TrashCompactor {
+    public static PrintStream out = utils.initializeOutputStream();
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<String> lines = utils.getInputLines();
+            long ans = 0;
+
+            long startTime = utils.getCurrentTime();
+            ArrayList<int[]> grid = new ArrayList<>();
+            for(int i=0; i<lines.size()-1; i++) {
+                String[] parts = lines.get(i).trim().split("\\s+");
+
+                int[] numbers = Arrays.stream(parts).mapToInt(Integer::parseInt).toArray();
+                grid.add(numbers);
+            }
+
+            String[] operators = lines.getLast().trim().split("\\s+");
+            int n = grid.size(), m = grid.getFirst().length;
+            for(int i=0; i<m; i++) {
+                long columnValue = (operators[i].equals("*")) ? 1 : 0; 
+                for(int j=0; j<n; j++) {
+                    int currentNumber = grid.get(j)[i];
+
+                    // out.printf("Column : %d, Cur : %d, Value : %d\n", i, currentNumber, columnValue);
+                    if(operators[i].equals("*"))
+                        columnValue *= currentNumber;
+                    else
+                        columnValue += currentNumber;
+                }
+
+                // out.printf("Column : %d, Value : %d\n", i, columnValue);
+                ans += columnValue;
+            }
+            
+            long endTime = utils.getCurrentTime();
+            utils.printTimeStats(out, startTime, endTime);
+
+            out.printf("Asnwer : %d\n", ans);
+            System.out.println("Answer : " + ans);
+        } catch(Exception e) {
+            System.out.println("Exception : " + e.getMessage());
+            e.printStackTrace(out);
+        }
+    }
+}
